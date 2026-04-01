2d4ecd0d393de1db18008b84c9c0bc54eaf6d728
karimovazamatjon14-art <karimov.azamatjon14@gmail.com>
2026-04-01 18:51:10 +0500

    Update python2.py

--- python2.py
+++ python2.py
@@ -11,6 +11,7 @@ if answer == "yes":
     print("LETS PLAY:",name)
 else:
     print("OH OK BUT YOU WILL:)")
+Azamat_game_points = 0
 print("GUES THE NUMBER")
 print("you shold guess the number")
 print("at start you have 10 tries on 1 game")
@@ -26,6 +27,7 @@ print("         .")
 
 print("EASY ROUND")
 print("C")
+print("WIN TO TAKE 3 Azamat_game_points")
 import random
 print("                          ")
 number = random.randint(1,50)
@@ -41,6 +43,7 @@ while tries > 0:
         print("small")
     else:
         print("you won:",name)
+        Azamat_game_points += 3
         break
     print("you have",tries,"tries")
 if tries == 0:
@@ -52,6 +55,7 @@ print("again")
 
 print("MIDLE MEDIUM ROUND")
 print("C+")
+print("WIN TO TAKE 6 Azamat_game_points")
 import random
 print("                          ")
 number = random.randint(1,100)
@@ -67,6 +71,7 @@ while tries > 0:
         print("small")
     else:
         print("you won:",name)
+        Azamat_game_points += 6
         break
     print("you have",tries,"tries")
 if tries == 0:
@@ -74,9 +79,9 @@ if tries == 0:
 print("again")
 
 
-
 print(" MEDIUM ROUND")
 print("B")
+print("WIN TO TAKE 10 Azamat_game_points")
 import random
 print("                          ")
 number = random.randint(1,500)
@@ -92,6 +97,7 @@ while tries > 0:
         print("small")
     else:
         print("you won:",name)
+        Azamat_game_points += 10
         break
     print("you have",tries,"tries")
 if tries == 0:
@@ -102,7 +108,7 @@ print("again")
 
 print("!HARD ROUND!")
 print("A")
-
+print("WIN TO TAKE 3=15 Azamat_game_points")
 import random
 print("                          ")
 number = random.randint(1,1000)
@@ -118,6 +124,7 @@ while tries > 0:
         print("small")
     else:
         print("you won:",name)
+        Azamat_game_points += 15
         break
     print("you have",tries,"tries")
 if tries == 0:
@@ -127,7 +134,7 @@ print("again")
 
 print("!!SUPER HARD ROUND!!")
 print("S")
-
+print("WIN TO TAKE 18 Azamat_game_points")
 import random
 print("                          ")
 number = random.randint(1,2000)
@@ -143,6 +150,7 @@ while tries > 0:
         print("small")
     else:
         print("you won:",name)
+        Azamat_game_points += 18
         break
     print("you have",tries,"tries")
 if tries == 0:
@@ -154,7 +162,7 @@ print("again")
 
 print("!!!EXTREME ROUND!!!")
 print("SS")
-
+print("WIN TO TAKE 35 Azamat_game_points")
 import random
 print("                          ")
 number = random.randint(1,5000)
@@ -170,6 +178,7 @@ while tries > 0:
         print("small")
     else:
         print("you won:",name)
+        Azamat_game_points += 35
         break
     print("you have",tries,"tries")
 if tries == 0:
@@ -183,7 +192,7 @@ print("again")
 
 print("!!!INPOSSIBLE ROUND!!!")
 print("SS+")
-
+print("WIN TO TAKE 100 Azamat_game_points")
 import random
 print("                          ")
 number = random.randint(1,100000)
@@ -199,13 +208,60 @@ while tries > 0:
         print("small")
     else:
         print("you won:",name)
+        Azamat_game_points += 100
         break
     print("you have",tries,"tries")
 if tries == 0:
     print("you lose number was:", number)
 print("again")
 
+print("!!!NEW GAME!!!")
+
+tries = 5
+stage = 1
+print("YOU HAVE 50% CHANSE TO CHOOSE CORRECT NUMBER")
+print("!!!THE GAME HAS 50 STAGES!!!")
+print("WIN TO GET 200 Azamat_game_points")
+while tries > 0:
+    import random
+    number = random.randint(1,2)
+    n = int(input("1 or 2:"))
+    if n == number:
+        print("!!!!!!!GREAT!!!!!!")
+        
+    else:
+        print("!!!!NOOOOOOO!!!!")
+        tries -= 1
+    stage += 1
+    print("YOU ARE ON",stage,"STAGE")
+    print("YOU HAVE",tries,"TRIES")
+if tries == 0:
+    print("GAME OVER YOU WAS ON",stage,"STAGE")
+if stage == 50:
+    print("you won")
+    Azamat_game_points += 200
+   
+
+
+
+
+
+
+
+
+
+
+
 
 
 print("GAMES END, NEW GAMES BE SOON")
 print("BYE",name)
+
+
+
+
+
+
+
+
+
